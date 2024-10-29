019e78ed2c4c7616b41460a8cb36c85aa279b5da
Jean Colil <jfrancocolil@gmail.com>
2024-10-29 00:24:18 -0300

    Se agregor Bcrypt

--- Aplicacion/__init__.py
+++ Aplicacion/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+
+bcrypt = Bcrypt()
 
 def run_app():
     
     app = Flask(__name__)
+    
+    bcrypt.init_app(app)
     
     app.config.from_object('config.Config')
     
--- Aplicacion/auth.py
+++ Aplicacion/auth.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, redirect, request, g, session, Blueprint, url_for, flash
-from werkzeug.security import generate_password_hash, check_password_hash
 
 from .models import User
 
-from Aplicacion import db
+from Aplicacion import db, bcrypt
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -14,8 +13,9 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+        password_bcrypt = bcrypt.generate_password_hash(password)
         
-        user = User(username, generate_password_hash(password))
+        user = User(username, password=password_bcrypt)
         
         user_name = User.query.filter_by(username=username).first()
         
@@ -40,10 +40,10 @@ def login():
         
         user = User.query.filter_by(username=username).first()
         
-        if username is None or not check_password_hash(user.password, password):
+        if user == None or not bcrypt.check_password_hash(user.password, password):
             error = "El Usuario o Contraseña son ERRONEAS"
         
-        if error == None:
+        if error is None:
             session.clear()
             session['user_id'] = user.id
             return redirect(url_for('index'))

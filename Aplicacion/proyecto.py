0273e1bba7b7aab205c7ee66cdd53970ad4fe10f
Jean Colil <jfrancocolil@gmail.com>
2024-10-31 00:21:09 -0300

    Se modifico el inicio de sesion y se creo el Backend de Tarea

--- Aplicacion/proyecto.py
+++ Aplicacion/proyecto.py
@@ -2,11 +2,20 @@ from flask import Flask, render_template, request, Blueprint, redirect, url_for,
 
 from Aplicacion import db
 
-from .models import Proyecto
+from .models import Proyecto, User
 
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 bp = Blueprint("proyecto", __name__, url_prefix="/proyecto")
+
+
+
+@bp.route('/ver-proyectos', methods=['GET', 'POST'])
+@login_required
+def ver_proyectos():
+    proyectos = Proyecto.query.filter_by(created_by=current_user.id).all()
+    return render_template('proyecto/proyectos.html', proyecto=proyectos)
+
 
 
 @bp.route('/register-proyecto', methods=['GET', 'POST'])
@@ -18,20 +27,14 @@ def register_proyecto():
         desc = request.form['desc']
         state = request.form['state']
         
-        proyecto = Proyecto(title, desc, state)
+        proyecto = Proyecto(title, desc, state, current_user.id ,)
         
         db.session.add(proyecto)
         db.session.commit()
-        return redirect(url_for('index.html'))
+        flash(f"El proyecto fue creado correctamente")
+        return redirect(url_for('index'))
     
     return render_template('proyecto/register.html')
-
-
-@bp.route('/ver-proyectos', methods=['GET', 'POST'])
-@login_required
-def ver_proyectos():
-    proyectos = Proyecto.query.all()
-    return render_template('proyecto/proyectos.html', proyectos=proyectos)
 
 
 def tener_id(id):
